faf7d622f44a4cd901348adaf41d4e119d2baedb
Frederickkrisna <frederickkrisna@gmail.com>
2024-07-20 20:02:29 +0700

    initial commit

---
+++ App.py
@@ -0,0 +1,80 @@
+import os
+import pickle
+import nltk
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+def add_review():
+    select = 0
+    review = ""
+    review_category = ""
+    while True:
+        os.system("cls")
+        print("1. ADD REVIEW")
+        print("2. BACK")
+        select = int(input(">> "))
+        if select == 1:
+            review = input("ENTER REVIEW: ")
+            review = review.lower()
+            review = word_tokenize(review)
+            try:
+                file = open("model.pickle", "rb")
+                classifier = pickle.load(file)
+                review_category = classifier.classify(FreqDist(review))
+                print("REVIEW CATEGORY: " + review_category)
+                file.close()
+            except FileNotFoundError:
+                print("ERROR: CLASSIFIER NOT FOUND.")
+                print("PLEASE TRAIN THE CLASSIFIER FIRST.")
+                a = input('').split(" ")[0]
+            except Exception as e:
+                print("ERROR: " + e)
+                a = input('').split(" ")[0]
+                return
+            write_file(review, review_category)
+            print("REVIEW ADDED SUCCESSFULLY.")
+            a = input('').split(" ")[0]
+        elif select == 2:
+            break
+        else:
+            print("INVALID INPUT. PLEASE TRY AGAIN.")
+            a = input('').split(" ")[0]
+            continue
+
+def main_menu():
+    select = 0
+    while True:
+        review = open("reviews.txt", "r").read()
+        review = sent_tokenize(review)
+        review_category = ""
+        review_text = ""
+        if len(review) > 0:
+            review_text = review[-1].split("#")[1]
+            review_category = review[-1].split("#")[0]
+        else:
+            review = ""
+        os.system("cls")
+        print("SMARTPHONE BRAND RECOMMENDATION APPLICATION BASED ON REVIEW")
+        print(f"YOUR REVIEW: {review_text}")
+        print(f"YOUR REVIEW CATEGORY (POSITIVE/NEGATIVE): {review_category}")
+        print("1. ADD REVIEW")
+        print("2. VIEW SMARTPHONE BRAND RECOMMENDATION")
+        print("3. VIEW NAMED ENTITY RECOGNITION")
+        print("4. EXIT")
+        select = int(input(">> "))
+        if select == 1:
+            add_review()
+        elif select == 2:
+            print("VIEW SMARTPHONE BRAND RECOMMENDATION")
+            view_recommendation()
+        elif select == 3:
+            print("VIEW NAMED ENTITY RECOGNITION")
+            view_named_entity_recognition()
+        elif select == 4:
+            break
+        else:
+            print("INVALID INPUT. PLEASE TRY AGAIN.")
+            continue
+
+if __name__ == "__main__":
+    main_menu()
